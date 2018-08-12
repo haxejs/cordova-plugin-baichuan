477399bfce8db314e57932bd1a1a338fb27924db
kmshi_array <kmshi_array@yahoo.com>
2018-08-12 16:18:35 +0800

    refactor code

--- src/ios/BaichuanPlugin.m
+++ src/ios/BaichuanPlugin.m
@@ -25,6 +25,55 @@
     [[AlibcTradeSDK sharedInstance] setDebugLogOpen:NO];
 }
 
+/* NOTE: calls into JavaScript must not call or trigger any blocking UI, like alerts */
+- (void)handleOpenURL:(NSNotification*)notification
+{
+    // override to handle urls sent to your app
+    // register your url schemes in your App-Info.plist
+    
+    NSURL* url = [notification object];
+    
+    if ([url isKindOfClass:[NSURL class]]) {
+        [[AlibcTradeSDK sharedInstance] application:[UIApplication sharedApplication]
+                                            openURL:url
+                                            options:nil];
+    }
+}
+
+/*
+ NOTE: calls into JavaScript must not call or trigger any blocking UI, like alerts
+ */
+- (void)handleOpenURLWithApplicationSourceAndAnnotation: (NSNotification*)notification
+{
+    
+    // override to handle urls sent to your app
+    // register your url schemes in your App-Info.plist
+    
+    // The notification object is an NSDictionary which contains
+    // - url which is a type of NSURL
+    // - sourceApplication which is a type of NSString and represents the package
+    // id of the app that calls our app
+    // - annotation which a type of Property list which can be several different types
+    // please see https://developer.apple.com/library/content/documentation/General/Conceptual/DevPedia-CocoaCore/PropertyList.html
+    
+    NSDictionary*  notificationData = [notification object];
+    
+    if ([notificationData isKindOfClass: NSDictionary.class]){
+        
+        NSURL* url = notificationData[@"url"];
+        NSString* sourceApplication = notificationData[@"sourceApplication"];
+        id annotation = notificationData[@"annotation"];
+        
+        if ([url isKindOfClass:NSURL.class] && [sourceApplication isKindOfClass:NSString.class] && annotation) {
+            [[AlibcTradeSDK sharedInstance] application:[UIApplication sharedApplication]
+                                                openURL:url
+                                      sourceApplication:sourceApplication
+                                             annotation:annotation];
+        }
+    }
+}
+
+
 - (AlibcTradeTaokeParams *)getTaokeParams:(NSDictionary *)taokeArgs {
     if (!taokeArgs) {
         return nil;
